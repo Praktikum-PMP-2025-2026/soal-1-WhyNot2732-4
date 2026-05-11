a11243bcea981115adfbac5ca803755704921136
dam27 <91896848+WhyNot2732@users.noreply.github.com>
2026-05-11 08:48:22 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,64 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : 5
+ *   Hari dan Tanggal    : Senin, 11 Mei 2026
+ *   Nama (NIM)          : R. Damar Prawiro Kusumo Sudradjat (13224109)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Menghitung derajat setiap simpul, simpul yang memiliki derajat maksimum, dan mencari simpul yang terisolasi
+ * dari input user berupa matriks adjacency 
+ */
 
-int main(void) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int N;
+    char input[100];
+    char* tok;
+    int temp, max, maxId, iso;
+    max = -1;
+    iso = -1;
+
+    fgets(input, 100, stdin);
+    tok = strtok(input, " ");
+    N = atoi(tok);
+
+    int mat[N][N];
+
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            tok = strtok(NULL, " ");
+            mat[i][j] = atoi(tok);
+        }
+    }
+    
+    for (int i = 0; i < N; i++){
+        temp = 0;
+        for (int j = 0; j < N; j++){
+            temp += mat[i][j];
+        }
+        printf("DEGREE %d %d\n", i, temp);
+        if (max < temp){
+            maxId = i;
+            max = temp;
+        }
+        if (temp == 0){
+            iso = i;
+        }
+    }
+
+    printf("MAX_VERTEX %d\n", maxId);
+    if (iso > -1){
+        printf("ISOLATED %d\n", iso);
+    } else{
+        printf("ISOLATED NONE\n");
+    }
     return 0;
 }
+
+/*
+REFERENSI:
+https://www.geeksforgeeks.org/cpp/strtok-strtok_r-functions-c-examples/
+https://www.geeksforgeeks.org/c/implementation-of-graph-in-c/
+*/
